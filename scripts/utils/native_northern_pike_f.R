56190d73ce5e552de30051437284b8f812176c2f
Phelan <John.Phelan@gov.bc.ca>
2025-09-18 09:44:08 -0700

    remove extra path

--- scripts/utils/native_northern_pike_f.R
+++ scripts/utils/native_northern_pike_f.R
@@ -1,5 +1,5 @@
 remove_native_nPike<-function(x){
-  bc_watersheds <- sf::st_read(paste0(onedrive_wd,"CNF/watershed_groups.gpkg"), quiet = TRUE)
+  bc_watersheds <- sf::st_read(paste0(onedrive_wd,"watershed_groups.gpkg"), quiet = TRUE)
   bc_watersheds <- bc_watersheds |> sf::st_transform(4326)
   pike_watersheds<- bc_watersheds |> 
     dplyr::filter(stringr::str_detect(WATERSHED_GROUP_NAME, c("Peace|Liard|Yukon|Alsek|Taku|Makenzie")))
